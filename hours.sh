21fe53587872eab1411c7f2f1cde112096e490d8
Larkin Nickle <me@larbob.org>
2019-03-03 18:09:28 -0500

    hours.sh: enclose calls for srvfile in quotes

--- hours.sh
+++ hours.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 function addhrs {
 	echo "Date?"
-	read date  
+	read date
 	echo "Amount of hours?"
 	read hours
 	echo "Description?"
 	read description
-	echo "$date - $hours - $description" >> ${srvfile}
+	echo "$date - $hours - $description" >> "${srvfile}"
 }
 function rmhrs {
-	cat -n ${srvfile}
+	cat -n "${srvfile}"
 	echo "Remove which line?"
 	printf ":"
 	read line
-	sed "${line}d" ${srvfile} > ${srvfile}.tmp
+	sed "${line}d" "${srvfile}" > "${srvfile}".tmp
 	echo "The new service hour log will be as follows:"
-	cat -n ${srvfile}.tmp
+	cat -n "${srvfile}".tmp
 	echo "Does this look okay? (y/n)"
 	read resp
 	if [ "$resp" == "y" ]; then
-		mv ${srvfile}.tmp ${srvfile}
+		mv "${srvfile}".tmp "${srvfile}"
 	else
-		rm ${srvfile}.tmp
+		rm "${srvfile}".tmp
 	fi
 }
 function edithrs {
-	cat -n ${srvfile}
+	cat -n "${srvfile}"
 	echo "Edit which line?"
 	printf ":"
 	read line
-	awk "NR == $line" ${srvfile} > ${srvfile}.tmp
-	${EDITOR:=xdg-open} ${srvfile}.tmp
-	sed -i "${line}c\\$(cat ${srvfile}.tmp)" ${srvfile}
-	rm ${srvfile}.tmp
+	awk "NR == $line" "${srvfile}" > "${srvfile}".tmp
+	${EDITOR:=xdg-open} "${srvfile}".tmp
+	sed -i "${line}c\\$(cat "${srvfile}".tmp)" "${srvfile}"
+	rm "${srvfile}".tmp
 	echo "Finished editing."
 }
 function lshrs {
 	echo "Current service hours"
-	cat ${srvfile}
+	cat "${srvfile}"
 }
 function total {
 	tot=$((0))
@@ -45,15 +45,15 @@ function total {
 	do
 		temptot=$(echo $lines | awk '{print $4}')
 		tot=$(($tot + $temptot))
-	done < ${srvfile}
+	done < "${srvfile}"
 	echo "$tot"
 }
 if [ "$#" -ne 0 ]; then
 	srvfile="$1"
-	[ ! -f ${srvfile} ] && touch ${srvfile}
+	[ ! -f "${srvfile}" ] && touch "${srvfile}"
 else
 	srvfile=srvhrs
-	[ ! -f ${srvfile} ] && touch ${srvfile}
+	[ ! -f "${srvfile}" ] && touch "${srvfile}"
 fi
 lshrs
 while true 
